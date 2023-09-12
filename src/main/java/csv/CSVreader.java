9018cb6d2d55a1923d11ed11e91d1f5a6df91bf0
oskarjargilo1 <oskarjargilo1@gmail.com>
2023-09-12 20:15:39 +0100

    editable rows but not saved to file

--- src/main/java/csv/CSVreader.java
+++ src/main/java/csv/CSVreader.java
@@ -3,34 +3,31 @@ package csv;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 class CSVreader {
-    private final String filePath = "src/main/resources/MyFilms.csv";
-    private final File file;
     private Scanner filmsFile;
 
     public CSVreader() {
-        file = new File(filePath);
         try {
-            filmsFile = new Scanner(file, StandardCharsets.UTF_8);
-        } catch (FileNotFoundException e) {
+            URL filmsCSVfile = getClass().getClassLoader().getResource("txt/MyFilms.csv");
+            assert filmsCSVfile != null;
+            filmsFile = new Scanner(new File(filmsCSVfile.toURI()), StandardCharsets.UTF_8);
+        } catch (URISyntaxException | IOException e) {
             System.err.println("File not found.");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
         filmsFile.useDelimiter("[\n;]");
     }
 
     public String nextLine() {
-        if (hasNextLine())
-            return filmsFile.nextLine();
-        else
-            return "";
+        if (hasNextLine()) return filmsFile.nextLine();
+        else return "";
     }
 
-    public boolean hasNextLine(){
+    public boolean hasNextLine() {
         return filmsFile.hasNextLine();
     }
 
